bd753ac503ff6b6093ed020110a570b34a5a3d27
johndoesmama <johndoesmama@users.noreply.github.com>
2015-02-23 07:49:48 +0530

    add quicksort implementation for integer arrays

--- sort/src/sort.java
+++ sort/src/sort.java
@@ -27,7 +27,6 @@ public class sort {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
-		
 	}
 
 	public boolean less(int i, int j) {
@@ -123,7 +122,34 @@ public class sort {
 			for (int lo = 0; lo < N-sz; lo += sz+sz)
 				merge(a, aux, lo, lo+sz-1, Math.min(lo+sz+sz-1, N-1));
 	}
+
+	public int partition(int[] a, int lo, int hi) {
+		int i = lo, j = hi+1;
+		while (true) {
+			while (less(a[++i], a[lo]))
+				if (i == hi) break;
+			
+			while (less(a[lo], a[--j]))
+				if (j == lo) break;
+			
+			if (i >= j) break;
+			exch(a, i, j);
+		}
+		
+		exch(a, lo, j);
+		return j;
+	}
 	
+	public void quicksort(int[] a, int lo, int hi) {
+		if (hi <= lo) return;
+		int j = partition(a, lo, hi);
+		System.out.print("after partition...");
+		printArr(a);
+		System.out.println("");
+		quicksort(a, lo, j-1);
+		quicksort(a, j+1, hi);
+	}
+
 	public static void main(String[] args) {
 		
 		sort s = new sort();
@@ -135,6 +161,9 @@ public class sort {
 		//s.insertion(a);
 		//s.shellsort(a);
 		//s.mergesort(a);
-		s.BUmergesort(a);
-	}
+		//s.BUmergesort(a);
+		
+		StdRandom.shuffle(a);
+		s.quicksort(a, 0, a.length-1);
+	}	
 }
